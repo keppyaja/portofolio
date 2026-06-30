bbd3bda8b8ece1f638ac8643650a4d8d7849c10b
keppyaja <sukepaja18@gmail.com>
2026-07-01 01:02:28 +0700

    edit bat

--- update.bat
+++ update.bat
@@ -1,5 +1,12 @@
 @echo off
 
+echo ========================================
+echo Updating CyLab Stats...
+echo ========================================
+
 cd /d C:\Users\sukep\Documents\portofolio
 
 C:\Users\sukep\AppData\Local\Python\pythoncore-3.14-64\python.exe scripts\update_stats.py
+
+echo.
+echo Finished at %date% %time%
